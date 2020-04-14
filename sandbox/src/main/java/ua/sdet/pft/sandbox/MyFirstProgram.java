36a8cec34639d6f0c462ea7465c7b61e3afa32d8
Serhii Horoshko <sergii.horoshko@mev.com>
2020-04-14 21:39:35 +0300

    Added couple of functions with example

--- sandbox/src/main/java/ua/sdet/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/sdet/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,28 @@
 package ua.sdet.pft.sandbox;
 
 public class MyFirstProgram {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+    public static void main(String[] args) {
+    	hello("world");
+    	hello("user");
+    	hello("Serhii");
+
+        double len = 8.0;
+		System.out.println("Square area with side " + len + " = " + ares(len));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("rectangle area with sides" + a + b + " = " + area(a, b));
+    }
+
+    public static void hello(String x) {
+		System.out.println("Hello, " +  x + "!");
+	}
+
+	public static double ares(double l) {
+        return l * l;
+    }
+
+    public static double area(double a, double b) {
+    	return a * b;
 	}
 }
