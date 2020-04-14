2d9b7cb86f4d8d6de8cff8409e0e0be0047ba9f0
Serhii Horoshko <sergii.horoshko@mev.com>
2020-04-14 21:41:25 +0300

    code style

--- sandbox/src/main/java/ua/sdet/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/sdet/pft/sandbox/MyFirstProgram.java
@@ -2,27 +2,27 @@ package ua.sdet.pft.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-    	hello("world");
-    	hello("user");
-    	hello("Serhii");
+        hello("world");
+        hello("user");
+        hello("Serhii");
 
         double len = 8.0;
-		System.out.println("Square area with side " + len + " = " + ares(len));
+        System.out.println("Square area with side " + len + " = " + ares(len));
 
-		double a = 4;
-		double b = 6;
-		System.out.println("rectangle area with sides" + a + b + " = " + area(a, b));
+        double a = 4;
+        double b = 6;
+        System.out.println("rectangle area with sides" + a + b + " = " + area(a, b));
     }
 
     public static void hello(String x) {
-		System.out.println("Hello, " +  x + "!");
-	}
+        System.out.println("Hello, " + x + "!");
+    }
 
-	public static double ares(double l) {
+    public static double ares(double l) {
         return l * l;
     }
 
     public static double area(double a, double b) {
-    	return a * b;
-	}
+        return a * b;
+    }
 }
